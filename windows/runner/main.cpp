e14c9d92fd64249d27c5a806beb41f0ab2dfc65f
Llanas21 <joseluisllanas_21@outlook.com>
2025-11-23 18:47:18 -0600

    Se añadió función de que se inicie la app en pantalla completa para windows, corregido

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -25,8 +25,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
+  Win32Window::Point origin(GetSystemMetrics(SM_XVIRTUALSCREEN), GetSystemMetrics(SM_YVIRTUALSCREEN));
+  Win32Window::Size size(GetSystemMetrics(SM_CXVIRTUALSCREEN), GetSystemMetrics(SM_CYVIRTUALSCREEN));
+
   if (!window.Create(L"fanuc_focas_app", origin, size)) {
     return EXIT_FAILURE;
   }
